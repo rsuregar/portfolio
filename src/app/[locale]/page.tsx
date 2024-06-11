104f691f3c52cb4a30a51bddb6b2e5c063494c69
Léo Corre <lcorre.pro@gmail.com>
2024-06-12 00:13:12 +0200

    feat: 🚀 add particles, sync on logo theme

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,43 @@
+"use client";
 import About from "@/src/components/about";
 import Education from "@/src/components/education";
 import Footer from "@/src/components/footer";
 import Header from "@/src/components/header";
+import Particles from "@/src/components/magicui/particles";
 import TextReveal from "@/src/components/magicui/text-reveal";
 import Projects from "@/src/components/projects";
 import { useTranslations } from "next-intl";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Component() {
   const t = useTranslations("Hook");
+
+  const { theme } = useTheme();
+
+  const [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    setColor(theme === "dark" ? "#ffffff" : "#000000");
+  }, [theme]);
+
   return (
-    <main className="flex min-h-[100dvh] flex-col divide-y">
-      <Header></Header>
+    <main className="relative flex min-h-[100dvh] flex-col">
+      <Header />
       <section className="w-full" id="hook">
         <TextReveal text={t("title")} />
       </section>
-      <About></About>
-      <Education></Education>
-      <Projects></Projects>
-      <Footer></Footer>
+      <About />
+      <Education />
+      <Projects />
+      <Footer />
+      <Particles
+        className="absolute inset-0 -z-10 h-full w-full"
+        quantity={200}
+        ease={80}
+        color={color}
+        refresh
+      />
     </main>
   );
 }
